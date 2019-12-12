306fb58e6a4fcb3e22759d758edd0a8c7d22c670
Starz <58676682+starziepie@users.noreply.github.com>
2019-12-12 01:38:42 -0500

    Update registration_mainform.php

--- registration_mainform.php
+++ registration_mainform.php
@@ -27,7 +27,7 @@
                 	<div class="row">
                     	<div class="col-md-8 col-md-offset-2">
                         	<div class="header-text">
-                            	<h2>VA NAME HERE</h2>
+                            	<h2><?php echo SITE_NAME ?></h2>
                             	<h4>Register now and get ready for takeoff</h4>
                             	<hr>
                         	</div>
