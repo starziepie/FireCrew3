5ebd50883539cf4ac32fd41b42b634676cee9a7e
Starz <58676682+starziepie@users.noreply.github.com>
2019-12-12 00:52:19 -0500

    Update app_bottom.php

--- app_bottom.php
+++ app_bottom.php
@@ -2,8 +2,8 @@
                     <ul>
 
                         <li>
-                            <a href="https://community.infiniteflight.com/u/starz/">
-                                Crew Center Made with <i class="fa fa-heart" style="color:red;"></i> by Starz
+                            <a href="http://www.myselfparth.gq/">
+                                &copy; Parth Parth <script>document.write(new Date().getFullYear())</script>
                             </a>
                         </li>
                     </ul>
